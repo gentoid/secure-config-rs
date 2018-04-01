0ea4044c3e773896a232702fc744dff8ea238681
Viktor Lazarev <taurus101v@gmail.com>
2018-04-01 20:40:48 +0200

    Print out the cipher key and IV lenghts

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,14 @@ fn main() {
 
     // let iv = "000102030405060708090a0b0c0d0e0f".as_bytes();
 
+    let cipher = Cipher::aes_256_cfb1();
+
+    println!("Cipher key len: {:?}", cipher.key_len());
+    println!("Cipher IV len: {:?}", cipher.iv_len());
+
     let iv = b"\x00\x01\x02\x03\x04\x05\x06\x07\x00\x01\x02\x03\x04\x05\x06\x07";
 
-    let ciphertext = encrypt(Cipher::aes_256_cfb1(), key, Some(iv), a);
+    let ciphertext = encrypt(cipher, key, Some(iv), a);
 
     println!("{:?}", ciphertext);
 }
