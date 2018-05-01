ec7f0620a5c4aaa9c6da1a706e1e8bf919377417
Viktor Lazarev <taurus101v@gmail.com>
2018-05-01 19:52:20 +0200

    Add (not yet implemented) 'decode' subcommand

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,15 @@ fn main() {
                  .arg(Arg::with_name("string")
                         .required(true)
                         .help("A string to be encoded")))
+        .subcommand(SubCommand::with_name("decode")
+                 .about("decodes encoded string")
+                 .arg(Arg::with_name("string")
+                        .required(true)
+                        .help("A string to be decoded. It should be in the format of 'ENC(...)::IV(...)', otherwise it won't be decoded")))
         .get_matches();
 
-    let string = matches.subcommand_matches("encode").unwrap().value_of("string").unwrap();
+    let string_to_encode = matches.subcommand_matches("encode").unwrap().value_of("string").unwrap();
+    let string_to_decode = matches.subcommand_matches("decode").unwrap().value_of("string").unwrap();
 
     let key = b"000102030405060708090a0b0c0d0e0f";
 
@@ -30,7 +36,7 @@ fn main() {
         iv.push(c);
     }
 
-    let ciphertext = encrypt(cipher, key, Some(iv.as_bytes()), string.as_bytes()).unwrap();
+    let ciphertext = encrypt(cipher, key, Some(iv.as_bytes()), string_to_encode.as_bytes()).unwrap();
 
     let encoded = base64::encode(&ciphertext);
     let decoded = base64::decode(&encoded).unwrap();
